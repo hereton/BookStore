f8a2558453b4e0091a6367d00d7d2f62c6be094d
Totsapon Menkul <totsapon.m@ku.th>
2018-04-18 20:55:34 +0700

    add search feature

--- app/src/main/java/com/example/ton/bookstore/BookActivity.kt
+++ app/src/main/java/com/example/ton/bookstore/BookActivity.kt
@@ -1,50 +1,99 @@
 package com.example.ton.bookstore
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.SearchView
-import com.example.ton.bookstore.model.Book
-import com.example.ton.bookstore.model.BookRepository
-import com.example.ton.bookstore.model.WebBookRepository
 import com.example.ton.bookstore.presenter.BookPresenter
 import com.example.ton.bookstore.presenter.BookView
 import kotlinx.android.synthetic.main.activity_book.*
+import android.os.StrictMode
+import android.view.Menu
+import android.widget.BaseAdapter
+import com.example.ton.bookstore.model.*
+
 
 class BookActivity : AppCompatActivity(),BookView {
 
     private lateinit var bookPresenter: BookPresenter
     private lateinit var bookRepo: BookRepository
-    private lateinit var  adapter : ArrayAdapter<String>
-    private lateinit var booksDetail: ArrayList<String>
     private lateinit var searchView : SearchView
+    private lateinit var mainBooksList: List<Book>
+    private lateinit var bookAdapter: BookAdapter
+    private lateinit var task:TextLoader
 
     override fun setBookList(books: ArrayList<Book>) {
-        books.forEach { book -> booksDetail.add(book.toString())}
-        adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, booksDetail)
-        bookListView.adapter = adapter
+        mainBooksList = books
+        bookAdapter = BookAdapter(applicationContext,mainBooksList)
+        book_listView.adapter = bookAdapter
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
-        booksDetail = ArrayList()
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+
+        mainBooksList = ArrayList()
+
         bookRepo = WebBookRepository()
+//        bookRepo = MockBookRepository()
         bookPresenter = BookPresenter(this, bookRepo)
         bookPresenter.start()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         searchView = search_bar
-        searchView.queryHint = "Test"
-        println(searchView.query)
+        searchView.queryHint = "Search View Hint"
 
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                task = TextLoader(newText)
+                task.execute()
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+        })
+
+        return true
+    }
+
+
+    @SuppressLint("StaticFieldLeak")
+    inner class TextLoader(private var text:String): AsyncTask<String, String, BaseAdapter>(){
+
+        override fun doInBackground(vararg p0: String?): BaseAdapter {
+           val tempBookList:List<Book>
+
+            if(text != ""){
+                tempBookList = if(text.contains("[0-9]+".toRegex())){
+                    mainBooksList.filter { it -> it.publicationYear.toString().contains(text) }
+                }else{
+                    mainBooksList.filter { it -> it.title.toLowerCase().contains(text) }
+                }
+                return  BookAdapter(applicationContext,tempBookList)
+            }
+            return BookAdapter(applicationContext,mainBooksList)
+        }
+
+        override  fun onPostExecute(result:BaseAdapter){
+            book_listView.adapter = result
+        }
 
     }
 
-    fun onLoadButtonClicked(view:View){
-        bookPresenter.refresh()
-        adapter.clear()
 
-    }
+
+
 
 
 }
---
+++ app/src/main/java/com/example/ton/bookstore/model/BookAdapter.kt
@@ -0,0 +1,77 @@
+package com.example.ton.bookstore.model
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.LayoutInflater
+import com.example.ton.bookstore.R
+import java.io.InputStream
+import java.net.URL
+
+
+/**
+ * Created by ton on 12/4/18.
+ */
+class BookAdapter(
+        private var context:Context, private var booksList:List<Book>):BaseAdapter() {
+
+
+
+    private class ViewHolder(row: View?){
+        var textName = row?.findViewById<TextView>(R.id.name)
+        var ivImage = row?.findViewById<ImageView>(R.id.imgView)
+        var priceName = row?.findViewById<TextView>(R.id.price)
+
+
+    }
+
+
+    override fun getCount(): Int {
+       return this.booksList.size
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return p0.toLong()
+    }
+
+    override fun getItem(p0: Int): Any {
+        return this.booksList[p0]
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val view:View?
+            val viewHolder:ViewHolder
+        if(convertView == null){
+            val layout = LayoutInflater.from(context)
+            view = layout.inflate(R.layout.lv_item,parent,false)
+            viewHolder = ViewHolder(view)
+            view.tag = viewHolder
+        }else{
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+        val book = getItem(position) as Book
+        viewHolder.textName!!.text =  book.title
+        viewHolder.priceName!!.text = book.price.toString()
+//        try{
+//            val inputStream = URL(book.imageUrl).content as InputStream
+//            viewHolder.ivImage!!.setImageDrawable(Drawable.createFromStream(inputStream,"src name"))
+//
+//        }
+//        catch (e : Exception){
+//            println("no image url")
+//            val inputStream = URL("https://support.appsflyer.com/hc/article_attachments/115011109089/android.png").content as InputStream
+//            val d = Drawable.createFromStream(inputStream,"src name") as Drawable
+//            viewHolder.ivImage!!.setImageDrawable(d)
+//        }
+
+        return view as View
+    }
+
+
+
+}
