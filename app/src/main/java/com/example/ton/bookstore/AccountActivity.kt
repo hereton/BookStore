f192bef4ac8d0ba129b1b0ff5ef30f0bfeaedfa6
Totsapon Menkul <totsapon.m@ku.th>
2018-04-19 00:41:43 +0700

    done Phase 2

---
+++ app/src/main/java/com/example/ton/bookstore/AccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ton.bookstore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
--- app/src/main/java/com/example/ton/bookstore/BookActivity.kt
+++ app/src/main/java/com/example/ton/bookstore/BookActivity.kt
@@ -4,38 +4,52 @@ import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SearchView
+import android.support.v7.widget.SearchView
 import com.example.ton.bookstore.presenter.BookPresenter
 import com.example.ton.bookstore.presenter.BookView
 import kotlinx.android.synthetic.main.activity_book.*
 import android.os.StrictMode
 import android.view.Menu
-import android.widget.BaseAdapter
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.ton.bookstore.model.*
 
+class BookActivity : AppCompatActivity(),BookView,AdapterView.OnItemSelectedListener {
 
-class BookActivity : AppCompatActivity(),BookView {
+
 
     private lateinit var bookPresenter: BookPresenter
     private lateinit var bookRepo: BookRepository
     private lateinit var searchView : SearchView
     private lateinit var mainBooksList: List<Book>
+    private lateinit var tempBookList: List<Book>
     private lateinit var bookAdapter: BookAdapter
     private lateinit var task:TextLoader
+    private lateinit var sortByList: List<String>
+    private lateinit var spinnerView:Spinner
+    private  var sortStyle = 0
 
     override fun setBookList(books: ArrayList<Book>) {
         mainBooksList = books
+        mainBooksList = mainBooksList.sortedWith(compareBy(Book::title))
         bookAdapter = BookAdapter(applicationContext,mainBooksList)
         book_listView.adapter = bookAdapter
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_book)
 
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
+        //Create Sort by catagories
+        sortByList = listOf("A-Z","IncPublicYear")
+
+        tempBookList = listOf()
 
         mainBooksList = ArrayList()
 
@@ -47,7 +61,19 @@ class BookActivity : AppCompatActivity(),BookView {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        searchView = search_bar
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        val search = menu!!.findItem(R.id.action_search)
+        searchView = search.actionView as SearchView
+
+        val spinner = menu.findItem(R.id.spinner)
+        spinnerView = spinner.actionView as Spinner
+        spinnerView.onItemSelectedListener = this
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,sortByList)
+        spinnerView.adapter = adapter
+
+
         searchView.queryHint = "Search View Hint"
 
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
@@ -69,9 +95,9 @@ class BookActivity : AppCompatActivity(),BookView {
 
 
     @SuppressLint("StaticFieldLeak")
-    inner class TextLoader(private var text:String): AsyncTask<String, String, BaseAdapter>(){
+    inner class TextLoader(private var text:String): AsyncTask<String, String, List<Book>>(){
 
-        override fun doInBackground(vararg p0: String?): BaseAdapter {
+        override fun doInBackground(vararg p0: String?): List<Book> {
            val tempBookList:List<Book>
 
             if(text != ""){
@@ -80,17 +106,37 @@ class BookActivity : AppCompatActivity(),BookView {
                 }else{
                     mainBooksList.filter { it -> it.title.toLowerCase().contains(text) }
                 }
-                return  BookAdapter(applicationContext,tempBookList)
+                return  tempBookList
             }
-            return BookAdapter(applicationContext,mainBooksList)
+            return mainBooksList
         }
 
-        override  fun onPostExecute(result:BaseAdapter){
-            book_listView.adapter = result
+        override  fun onPostExecute(result:List<Book>){
+            tempBookList = result
+            book_listView.adapter = BookAdapter(applicationContext,sortList(sortStyle,result))
         }
 
     }
 
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, index: Int, p3: Long) {
+        if(index == 0) sortStyle = 0
+        if(index == 1) sortStyle = 1
+        if (tempBookList.isEmpty()){
+            book_listView.adapter = BookAdapter(applicationContext,sortList(sortStyle,mainBooksList))
+        }else{
+            book_listView.adapter = BookAdapter(applicationContext,sortList(sortStyle,tempBookList))
+        }
+
+    }
+    override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+    fun sortList(value:Int,unsortedList:List<Book>):List<Book>{
+        if (value == 0 ){
+            return unsortedList.sortedWith(compareBy(Book::title))
+        }
+        return unsortedList.sortedWith(compareBy(Book::publicationYear))
+
+    }
 
 
 
